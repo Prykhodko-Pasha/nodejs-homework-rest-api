53289da34a46655011d43a175c45ff50f0b1b02a
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-12-28 14:30:55 +0200

    feat: add mongodb

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 
 const contactsRouter = require("./routes/api/contacts");
 
